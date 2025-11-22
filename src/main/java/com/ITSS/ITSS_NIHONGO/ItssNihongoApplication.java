5a565047220b82de7f45b770046a1fd6684d6e40
Roku2004 <tanakaroku2004@gmail.com>
2025-11-22 21:19:26 +0700

    thu muc be

---
+++ src/main/java/com/ITSS/ITSS_NIHONGO/ItssNihongoApplication.java
@@ -0,0 +1,13 @@
+package com.ITSS.ITSS_NIHONGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItssNihongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItssNihongoApplication.class, args);
+	}
+
+}
