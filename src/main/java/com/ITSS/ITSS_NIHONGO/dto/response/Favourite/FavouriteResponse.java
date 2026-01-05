4c914d3da3f530e7318742a5a57b4af5c7f66d92
ディン・ディン・ロック <163549934+Roku2004@users.noreply.github.com>
2026-01-05 08:24:17 +0700

    Update FavouriteResponse.java

--- src/main/java/com/ITSS/ITSS_NIHONGO/dto/response/Favourite/FavouriteResponse.java
+++ src/main/java/com/ITSS/ITSS_NIHONGO/dto/response/Favourite/FavouriteResponse.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 @Builder
 public class FavouriteResponse {
     public int id;
+    public int dishId;
     public String dishesname;
     public int distance;
     public String imageUrl;
